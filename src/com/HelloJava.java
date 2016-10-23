211ec04a031d4151764bc621e92f0980c8a82a15
snsinfotech <rajeevthetriner@gmail.com>
2016-10-23 10:54:17 +0530

    adding junit report

---
+++ src/com/HelloJava.java
@@ -0,0 +1,22 @@
+package com;
+
+/**
+ * 
+ * @author Rajeev K Mohan
+ * @version 1,0
+ * @since Oct 11 , 2016
+ *
+ */
+public class HelloJava {
+
+    public static void main(String[] args) {
+	System.out.println("Hello Ant");
+
+    }
+
+    public double addMe(int x, int y) {
+
+	return x + y;
+    }
+
+}
---
+++ src/com/tests/TestCases.java
@@ -0,0 +1,16 @@
+package com.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCases {
+
+    @Test
+    public void test() {
+	//fail("Not yet implemented");
+	
+	assertEquals(20, 10+10);
+    }
+
+}
